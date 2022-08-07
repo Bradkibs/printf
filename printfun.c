1594a3ef10ccc3bd9b15a74cc97d64014009dcf9
Bradley Kibs <Bradleykibwana@gmail.com>
2022-08-07 20:31:23 +0300

    Created a void printf and printf function for task 0

---
+++ printfun.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * printf -
+ * @format:
+ * Return:
+ */
+int _printf(const char *format, ...)
+{
+	va_list ap;
+
+	va_start(ap, format);
+	_vprintf(format, ap);
+	va_end(ap);
+	return(0);
+
+}
---
+++ vprintfun.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * _vprintf - 
+ * @format:
+ * Return:
+ */
+void _vprintf(const char *format, va_list args)
+{
+	int state = 0;
+
+	char c;
+
+	char *s;
+
+	char *tmp;
+
+	while (*format)
+	{
+		if (state == 0)
+		{
+			if (*format == '%')
+				state = 1;
+			else
+				write(0, format, sizeof(char));
+				format++;
+		}
+		else 
+		{
+			if (state == 1)
+			{
+				switch(*format)
+				{
+					case 'c':
+						c = va_arg(args, int);
+						_putchar(c);
+						break;
+					case 's':
+						s = va_arg(args, char *);
+						while (*s)
+						{
+							_putchar(*s);
+							s++;
+						}
+						break;
+				}
+				format++;
+			}
+		}
+	}
+}
